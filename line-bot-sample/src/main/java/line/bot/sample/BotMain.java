78d0e4b5e2fd9723f52ce2d0dc5510516f094f7b
Y-Suzaki <geranium04.24.1981@gmail.com>
2017-06-23 20:22:44 +0900

    circleciでサンプルテストが実行されるように修正

---
+++ line-bot-sample/src/main/java/line/bot/sample/BotMain.java
@@ -0,0 +1,10 @@
+package line.bot.sample;
+
+public class BotMain {
+
+	public static void main(String[] args) {
+		System.out.println("** start **");
+
+	}
+
+}
---
+++ line-bot-sample/src/test/java/line/bot/sample/BotMainTest.java
@@ -0,0 +1,13 @@
+package line.bot.sample;
+
+import org.junit.Test;
+
+public class BotMainTest {
+
+	@Test
+	public void main() {
+		BotMain.main(null);
+
+		throw new RuntimeException();
+	}
+}
